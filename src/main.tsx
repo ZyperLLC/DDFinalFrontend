1df854d795e4a219cbdcec107594ade8db93f8b2
forrune <esoifeoma1@gmail.com>
2025-07-10 18:57:08 +0100

    Install New Packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -8,25 +9,27 @@ import Stake from './Stake';
 import Profile from './Profile';
 import Friend from './Friend';
 import './index.css';
-import {Toaster} from "react-hot-toast";
-import {UserProvider} from "./Context/UserContextProvider";
+import { Toaster } from "react-hot-toast";
+import { UserProvider } from "./Context/UserContextProvider";
 import { TonConnectUiProvider } from './Context/TonConnectUiContext';
+
+const manifestUrl = 'https://jade-imperial-hawk-534.mypinata.cloud/ipfs/bafybeiec6ililuj3yahb6wizc4gczrdbutwoyznj4ue5hkq7vr7fpkcpai/';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Toaster/>
-      <BrowserRouter>
-      <TonConnectUiProvider>
-      <UserProvider>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/stake" element={<Stake />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/friends" element={<Friend />} />
-        </Routes>
+    <Toaster />
+    <BrowserRouter>
+      <TonConnectUiProvider manifestUrl={manifestUrl}>
+        <UserProvider>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/stake" element={<Stake />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/friends" element={<Friend />} />
+          </Routes>
         </UserProvider>
-        </TonConnectUiProvider>
-      </BrowserRouter>
+      </TonConnectUiProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
