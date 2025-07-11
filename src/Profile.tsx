61acbe92d6139961ad2e680a3666b0d7fd93903b
sh4d0wy <sakshambhugra8@gmail.com>
2025-07-11 20:05:38 +0530

    chore: Trying to send amount in decimals

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -17,7 +17,6 @@ import './index.css';
 import { ConnectButton } from './components/ConnectButton';
 import { useContext } from 'react';
 import { UserContext } from './Context/UserContextProvider';
-import { fromNano } from '@ton/ton';
 
 export default function Profile() {
   const context = useContext(UserContext);
