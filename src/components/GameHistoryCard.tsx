7fcb1689c99b9db685248ef8a929db95b7c0b402
sh4d0wy <sakshambhugra8@gmail.com>
2025-07-13 21:42:13 +0530

    chore: Adding GameHistory from backend-8

--- src/components/GameHistoryCard.tsx
+++ src/components/GameHistoryCard.tsx
@@ -39,11 +39,11 @@ const GameHistoryCard: React.FC<Props> = ({ image, cost, prize, useTon, betId, r
       <img src={image} alt={"dolphin_image"} className="nft-image" />
       <div className="nft-info text-left">
         <p className="nft-detail">
-          <strong>{t('gameHistory.entryCost')}:</strong> {cost}
+          <strong>{t('gameHistory.entryCost')}: </strong> {cost}
           <img src={sourceImg} alt="Credit or TON" className="inline-block ml-2" width={20} height={20} />
         </p>
         <p className="nft-detail">
-          <strong>{t('gameHistory.prize')}:</strong> {prize}
+          <strong>{t('gameHistory.prize')}: </strong> {prize}
           <img src={sourceImg} alt="Credit or TON" className="inline-block ml-2" width={20} height={20} />
         </p>
       </div>
