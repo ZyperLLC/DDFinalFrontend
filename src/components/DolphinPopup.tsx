6ac860c05be84793e18fba27bb0b5881cb8de07f
forrune <esoifeoma1@gmail.com>
2025-07-13 02:04:12 +0100

    Update Dolphin Popup

--- src/components/DolphinPopup.tsx
+++ src/components/DolphinPopup.tsx
@@ -43,7 +43,7 @@ export default function DolphinPopup({
   }, []);
 
   return (
-    <div className="dophin-popup-image-bg" >
+    <div className="fixed inset-0 dophin-popup-image-bg" style={{ padding: '1rem' }}>
       <div
         style={{
           position: 'fixed',
