a75876ae5b8de6bac70c841a594047cc3d4e9d8b
sh4d0wy <sakshambhugra8@gmail.com>
2025-07-14 23:19:15 +0530

    chore: Fixed ui

--- src/components/StakeHeader.tsx
+++ src/components/StakeHeader.tsx
@@ -1,14 +1,12 @@
 import { useTranslation } from 'react-i18next';
-import { useTypewriter } from '../hooks/useTypeWriter';
 
 export default function StakeHeader() {
   const { t } = useTranslation();
-  const typedTitle = useTypewriter(t('stakeHeader.title'), 100, 1500); // use effect
 
   return (
     <div className="stake-card w-full max-w-4xl mx-auto">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center mt-0 mb-2">
-        {typedTitle}
+        {t('stakeHeader.title')}
         <span className="blinking-cursor">|</span>
       </h1>
       <div className="flex justify-center items-center">
