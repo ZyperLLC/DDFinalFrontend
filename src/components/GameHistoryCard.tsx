6e55bf2b4e3519666f202f6729a9bdc2012ac606
sh4d0wy <sakshambhugra8@gmail.com>
2025-07-15 16:35:10 +0530

    chore: Added drawId and startedAt

--- src/components/GameHistoryCard.tsx
+++ src/components/GameHistoryCard.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { getBettingRoundById } from '../api/userApi';
 import creditIcon from '../assets/credit.jpg';
 import tonSymbol from '../assets/ton_symbol.jpg';
+import { Bet } from '../types';
 
 interface Props {
   image: string;
@@ -16,14 +17,16 @@ interface Props {
 const GameHistoryCard: React.FC<Props> = ({ image, cost, prize, useTon, betId, result }:Props) => {
   const { t } = useTranslation();
   const [hasEnded,setHasEnded] = useState(false);
+  const [startedAt,setStartedAt] = useState<Date>(new Date());
   const sourceImg = useTon ?  tonSymbol: creditIcon;
-
+  
   useEffect(() => {
     const fetchBettingRound = async () => {
       if (betId) {
         try {
           const bettingRound = await getBettingRoundById(betId);
           setHasEnded(bettingRound.hasEnded);
+          setStartedAt(bettingRound.startedAt)
         } catch (error) {
           console.error("Error fetching betting round:", error);
         }
@@ -45,6 +48,14 @@ const GameHistoryCard: React.FC<Props> = ({ image, cost, prize, useTon, betId, r
         <p className="nft-detail">
           <strong>{t('gameHistory.prize')}: </strong> {prize}
           <img src={sourceImg} alt="Credit or TON" className="inline-block ml-2" width={20} height={20} />
+        </p>
+        <p className="nft-detail">
+          <strong>{t('gameHistory.drawId')}: </strong> {prize}
+          <span>{betId}</span>
+        </p>
+        <p className="nft-detail">
+          <strong>{t('gameHistory.startedAt')}: </strong> {prize}
+          <span>{startedAt?startedAt.toLocaleDateString() :'-'}</span>
         </p>
       </div>
       <div className={`result-tag ${result}`}>
--- src/types/index.ts
+++ src/types/index.ts
@@ -38,3 +38,5 @@ export interface Bet {
     stakedNFTs: StakedNFT[];
     friends: string[];
   }
+
+  
