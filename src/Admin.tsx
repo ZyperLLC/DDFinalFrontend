791fb289af24227424abb420fe888dcd22554142
sh4d0wy <sakshambhugra8@gmail.com>
2025-07-19 23:05:21 +0530

    chore:Fixed merge conflicts

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -12,55 +12,55 @@ import {
 } from './api/userApi';
 
 
-// import dolphin1 from './assets/dolphins/dolphin1.jpg';
-// import dolphin2 from './assets/dolphins/dolphin2.jpg';
-// import dolphin3 from './assets/dolphins/dolphin3.jpg';
-// import dolphin4 from './assets/dolphins/dolphin4.jpg';
-// import dolphin5 from './assets/dolphins/dolphin5.jpg';
-// import dolphin6 from './assets/dolphins/dolphin6.jpg';
-// import dolphin7 from './assets/dolphins/dolphin7.jpg';
-// import dolphin8 from './assets/dolphins/dolphin8.jpg';
-// import dolphin9 from './assets/dolphins/dolphin9.jpg';
-// import dolphin10 from './assets/dolphins/dolphin10.jpg';
-// import dolphin11 from './assets/dolphins/dolphin11.jpg';
-// import dolphin12 from './assets/dolphins/dolphin12.jpg';
-// import dolphin13 from './assets/dolphins/dolphin13.jpg';
-// import dolphin14 from './assets/dolphins/dolphin14.jpg';
-// import dolphin15 from './assets/dolphins/dolphin15.jpg';
-// import dolphin16 from './assets/dolphins/dolphin16.jpg';
-// import dolphin17 from './assets/dolphins/dolphin17.jpg';
-// import dolphin18 from './assets/dolphins/dolphin18.jpg';
-// import dolphin19 from './assets/dolphins/dolphin19.jpg';
-// import dolphin20 from './assets/dolphins/dolphin20.jpg';
-// import dolphin21 from './assets/dolphins/dolphin21.jpg';
-// import dolphin22 from './assets/dolphins/dolphin22.jpg';
-// import dolphin23 from './assets/dolphins/dolphin23.jpg';
-// import dolphin24 from './assets/dolphins/dolphin24.jpg';
-// import dolphin25 from './assets/dolphins/dolphin25.png';
-// import dolphin26 from './assets/dolphins/dolphin26.png';
-// import dolphin27 from './assets/dolphins/dolphin27.png';
-// import dolphin28 from './assets/dolphins/dolphin28.png';
-// import dolphin29 from './assets/dolphins/dolphin29.png';
-// import dolphin30 from './assets/dolphins/dolphin30.png';
-// import dolphin31 from './assets/dolphins/dolphin31.png';
-// import dolphin32 from './assets/dolphins/dolphin32.png';
-// import dolphin33 from './assets/dolphins/dolphin33.png';
-// import dolphin34 from './assets/dolphins/dolphin34.png';
-// import dolphin35 from './assets/dolphins/dolphin35.png';
-// import dolphin36 from './assets/dolphins/dolphin36.png';
+import dolphin1 from './assets/dolphins/dolphin1.jpg';
+import dolphin2 from './assets/dolphins/dolphin2.jpg';
+import dolphin3 from './assets/dolphins/dolphin3.jpg';
+import dolphin4 from './assets/dolphins/dolphin4.jpg';
+import dolphin5 from './assets/dolphins/dolphin5.jpg';
+import dolphin6 from './assets/dolphins/dolphin6.jpg';
+import dolphin7 from './assets/dolphins/dolphin7.jpg';
+import dolphin8 from './assets/dolphins/dolphin8.jpg';
+import dolphin9 from './assets/dolphins/dolphin9.jpg';
+import dolphin10 from './assets/dolphins/dolphin10.jpg';
+import dolphin11 from './assets/dolphins/dolphin11.jpg';
+import dolphin12 from './assets/dolphins/dolphin12.jpg';
+import dolphin13 from './assets/dolphins/dolphin13.jpg';
+import dolphin14 from './assets/dolphins/dolphin14.jpg';
+import dolphin15 from './assets/dolphins/dolphin15.jpg';
+import dolphin16 from './assets/dolphins/dolphin16.jpg';
+import dolphin17 from './assets/dolphins/dolphin17.jpg';
+import dolphin18 from './assets/dolphins/dolphin18.jpg';
+import dolphin19 from './assets/dolphins/dolphin19.jpg';
+import dolphin20 from './assets/dolphins/dolphin20.jpg';
+import dolphin21 from './assets/dolphins/dolphin21.jpg';
+import dolphin22 from './assets/dolphins/dolphin22.jpg';
+import dolphin23 from './assets/dolphins/dolphin23.jpg';
+import dolphin24 from './assets/dolphins/dolphin24.jpg';
+import dolphin25 from './assets/dolphins/dolphin25.png';
+import dolphin26 from './assets/dolphins/dolphin26.png';
+import dolphin27 from './assets/dolphins/dolphin27.png';
+import dolphin28 from './assets/dolphins/dolphin28.png';
+import dolphin29 from './assets/dolphins/dolphin29.png';
+import dolphin30 from './assets/dolphins/dolphin30.png';
+import dolphin31 from './assets/dolphins/dolphin31.png';
+import dolphin32 from './assets/dolphins/dolphin32.png';
+import dolphin33 from './assets/dolphins/dolphin33.png';
+import dolphin34 from './assets/dolphins/dolphin34.png';
+import dolphin35 from './assets/dolphins/dolphin35.png';
+import dolphin36 from './assets/dolphins/dolphin36.png';
 import toast from 'react-hot-toast';
 import { Bet } from './types';
 import { fromNano } from '@ton/ton';
 
 
-// const dolphinImages: { [key: number]: any } = {
-//   1: dolphin1, 2: dolphin2, 3: dolphin3, 4: dolphin4, 5: dolphin5, 6: dolphin6,
-//   7: dolphin7, 8: dolphin8, 9: dolphin9, 10: dolphin10, 11: dolphin11, 12: dolphin12,
-//   13: dolphin13, 14: dolphin14, 15: dolphin15, 16: dolphin16, 17: dolphin17, 18: dolphin18,
-//   19: dolphin19, 20: dolphin20, 21: dolphin21, 22: dolphin22, 23: dolphin23, 24: dolphin24,
-//   25: dolphin25, 26: dolphin26, 27: dolphin27, 28: dolphin28, 29: dolphin29, 30: dolphin30,
-//   31: dolphin31, 32: dolphin32, 33: dolphin33, 34: dolphin34, 35: dolphin35, 36: dolphin36
-// };
+const dolphinImages: { [key: number]: any } = {
+  1: dolphin1, 2: dolphin2, 3: dolphin3, 4: dolphin4, 5: dolphin5, 6: dolphin6,
+  7: dolphin7, 8: dolphin8, 9: dolphin9, 10: dolphin10, 11: dolphin11, 12: dolphin12,
+  13: dolphin13, 14: dolphin14, 15: dolphin15, 16: dolphin16, 17: dolphin17, 18: dolphin18,
+  19: dolphin19, 20: dolphin20, 21: dolphin21, 22: dolphin22, 23: dolphin23, 24: dolphin24,
+  25: dolphin25, 26: dolphin26, 27: dolphin27, 28: dolphin28, 29: dolphin29, 30: dolphin30,
+  31: dolphin31, 32: dolphin32, 33: dolphin33, 34: dolphin34, 35: dolphin35, 36: dolphin36
+};
 
 const ADMIN_WALLETS = import.meta.env.VITE_ADMIN_WALLET;
 
@@ -71,7 +71,7 @@ export default function AdminPage() {
   const walletAddress = context?.user.walletAddress;
   const [isAuthorized, setIsAuthorized] = useState(false);
   const [activeFilter, setActiveFilter] = useState<'all' | 'ton' | 'credits'>('all');
-  const [_, setUserBets] = useState<any[]>([]);
+  const [userBets, setUserBets] = useState<any[]>([]);
   const [currentRound, setCurrentRound] = useState<any>(null);
   const [resultNumber, setResultNumber] = useState<string>('');
   const [checkedBets, setCheckedBets] = useState<any[]>([]);
@@ -127,6 +127,7 @@ export default function AdminPage() {
   };
 
   fetchCurrentRound();
+  console.log(userBets);
 }, []);
 
 
@@ -215,7 +216,15 @@ const handleStartRound = async ()=>{
           <table className="admin-table w-full mb-4 text-white">
             <thead><tr><th>No.</th><th>NFT</th><th>Total</th><th>TON</th><th>Credit</th></tr></thead>
             <tbody>
-              
+              {userBets.map((b:any)=>(
+                <>
+                <td>{b.nftId}</td>
+                <td><img src={dolphinImages[b.nftId]} width="40px"/></td>
+                <td>{b.amount}</td>
+                <td>{b.tonAmount}</td>
+                <td>{b.amount-b.tonAmount}</td>
+                </>
+              ))}
             </tbody>
           </table>
         </details>
