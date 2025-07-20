f3a266d1bffe50e2008933373cda73ec8a8adbda
sh4d0wy <sakshambhugra8@gmail.com>
2025-07-20 23:15:07 +0530

    chore: Changing the styles of admin page

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -187,9 +187,9 @@ const handleCheckResult = () => {
       <div className="flex flex-col items-center text-center">
         <img src={logo} alt="Logo" className="animated-logo mb-14" style={{ width: '250px' }} />
         <h1 className="text-3xl font-bold mb-6">Admin Section</h1>
-        <div className="flex flex-wrap justify-center gap-4 mb-12 flex-col align-center">
+        <div className="flex flex-wrap justify-center gap-6 mb-12 flex-col align-center">
           <button className="admin-btn" onClick={stopCurrentRound}>Stop Betting</button>
-          <input type="number" min={1} max={36} placeholder='Type Winning No.' onChange={(e)=>setWinningNumber(Number(e.target.value))}/>
+          <input type="number" min={1} max={36} placeholder='Type Winning No.' onChange={(e)=>setWinningNumber(Number(e.target.value))} style={{borderRadius:"10px",padding:"10px 5px"}}/>
           <button className="admin-btn" onClick={handleEndRound}>End Round</button>
         </div>
       </div>
