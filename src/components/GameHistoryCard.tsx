e7ba8435406ec9423babfae68c03454e009ffa25
sh4d0wy <sakshambhugra8@gmail.com>
2025-07-23 22:57:06 +0530

    Chore: fixed teh welcome popup

--- src/components/GameHistoryCard.tsx
+++ src/components/GameHistoryCard.tsx
@@ -51,7 +51,7 @@ const GameHistoryCard: React.FC<Props> = ({ image, cost, prize, useTon, betId, r
           <img src={sourceImg} alt="Credit or TON" className="inline-block ml-2" width={20} height={20} />
         </p>
         <p className="nft-detail">
-          <strong>{t('gameHistory.prize')}: </strong> {prize}
+          <strong>{t('gameHistory.prize')}: </strong> {Number(prize).toFixed(2)}
           <img src={sourceImg} alt="Credit or TON" className="inline-block ml-2" width={20} height={20} />
         </p>
         <p className="nft-detail">
