f1c3cf3455d64cf719f7bba3c04adb9ea2f22177
forrune <esoifeoma1@gmail.com>
2025-07-24 02:09:59 +0100

    Update Draw Loader

--- src/components/DrawLoader.tsx
+++ src/components/DrawLoader.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import background from './assets/background3.png'
+import background from '../assets/background3.png'
 
 function DrawLoader() {
   const [progress, setProgress] = useState(0)
