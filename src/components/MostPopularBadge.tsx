c5c28c6336aa2c82557bad6f558cde8220904157
forrune <esoifeoma1@gmail.com>
2025-08-05 03:52:30 +0100

    Update Most Popular Badge

--- src/components/MostPopularBadge.tsx
+++ src/components/MostPopularBadge.tsx
@@ -2,29 +2,24 @@ import { motion } from "framer-motion";
 
 const MostPopularBadge = () => {
   return (
-    <motion.div
-      initial={{ backgroundPosition: "0% 50%" }}
-      animate={{ backgroundPosition: "200% 50%" }}
-      transition={{
-        duration: 1,
-        repeat: Infinity,
-        ease: "linear",
-        repeatType: "loop",
-      }}
-      className="rounded-full px-4 py-1 text-sm font-semibold
-        bg-[linear-gradient(to_right,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FDBFE,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FDBFE)]
-        bg-[length:200%] text-white border-2 border-transparent
-        [background-clip:border-box] [mask-image:_linear-gradient(#fff_0_0)]
-        shadow-md inline-block"
-    >
-      <span
-        className="bg-black px-3 py-0.5 rounded-full text-transparent
-        bg-clip-text bg-[linear-gradient(to_right,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FDBFE)]
-        [background-size:200%] [-webkit-background-clip:text]"
-      >
+    <div className="relative inline-block rounded-full p-[2px]">
+      <motion.div
+        initial={{ backgroundPosition: "0% 50%" }}
+        animate={{ backgroundPosition: "200% 50%" }}
+        transition={{
+          duration: 1,
+          repeat: Infinity,
+          ease: "linear",
+          repeatType: "loop",
+        }}
+        className="absolute inset-0 rounded-full
+          bg-[linear-gradient(to_right,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FDBFE,#F87AFF)]
+          bg-[length:200%_200%] animate-gradient z-[-1]"
+      />
+      <span className="relative px-4 py-1 text-sm font-semibold text-white bg-black rounded-full">
         Most Popular
       </span>
-    </motion.div>
+    </div>
   );
 };
 
