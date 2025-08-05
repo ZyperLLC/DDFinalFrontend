cb216e5e93ad6278173dd29121ce807dc64884b4
forrune <esoifeoma1@gmail.com>
2025-08-05 06:12:26 +0100

    Update Button

--- src/components/MostPopularBadge.tsx
+++ src/components/MostPopularBadge.tsx
@@ -15,7 +15,7 @@ const MostPopularBadge = () => {
         bg-[linear-gradient(to_right,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FDBFE,#F87AFF)]
         bg-[length:200%_200%] relative"
     >
-      <div className="rounded-full bg-black px-4 py-1 text-sm font-semibold text-white">
+      <div className="rounded-full bg-black px-4 py-1 text-sm font-bold text-white">
         Most Popular
       </div>
     </motion.div>
