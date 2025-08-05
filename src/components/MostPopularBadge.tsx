2f62ec138070ada033edcd106672754654311fc2
forrune <esoifeoma1@gmail.com>
2025-08-05 07:16:44 +0100

    Update Most Popular Badge

--- src/components/MostPopularBadge.tsx
+++ src/components/MostPopularBadge.tsx
@@ -6,16 +6,16 @@ const MostPopularBadge = () => {
       initial={{ backgroundPosition: "0% 50%" }}
       animate={{ backgroundPosition: "200% 50%" }}
       transition={{
-        duration: 1,
+        duration: 1.5,
         repeat: Infinity,
         ease: "linear",
         repeatType: "loop",
       }}
       className="inline-block p-[2px] rounded-full
-        bg-[linear-gradient(to_right,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FDBFE,#F87AFF)]
+        bg-[linear-gradient(to_right,#2a2a2a,#3a3a3a,#1e1e1e,#2a2a2a)]
         bg-[length:200%_200%] relative"
     >
-      <div className="rounded-full bg-black px-4 py-1 text-sm font-bold text-white">
+      <div className="rounded-full bg-black/20 px-4 py-1 text-sm font-bold text-white">
         Most Popular
       </div>
     </motion.div>
