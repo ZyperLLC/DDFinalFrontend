c97e3b8e0cff67f19772bc89c978cec91f5c1622
forrune <esoifeoma1@gmail.com>
2025-08-06 17:31:09 +0100

    Update Badge

--- src/components/MostPopularBadge.tsx
+++ src/components/MostPopularBadge.tsx
@@ -15,9 +15,10 @@ const MostPopularBadge = () => {
         bg-[linear-gradient(90deg,#f72585,#7209b7)]
         bg-[length:200%_200%] relative"
     >
-      <div className="rounded-full bg-black/20 px-16 text-xs font-semibold text-white">
-        <h3>Most Popular</h3>
-      </div>
+       <div className="rounded-full bg-black/20 px-5 py-1 text-[10px] font-semibold text-white">
+           Most Popular
+       </div>
+
     </motion.div>
   );
 };
