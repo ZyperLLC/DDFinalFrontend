496e1568f101e1a818376deabb25b2bf3886c7e5
sh4d0wy <sakshambhugra8@gmail.com>
2025-09-03 15:43:10 +0530

    chore: testing

--- src/components/Withdrawpopup.tsx
+++ src/components/Withdrawpopup.tsx
@@ -11,6 +11,7 @@ import toast from 'react-hot-toast';
 import { slideUpFade } from '../utils/animations';
 import { useWithdrawDeposits } from '../hooks/useWithdrawDeposits';
 import { UserContext } from '../Context/UserContextProvider';
+import { Address } from '@ton/core';
 
 type Props = {
   id: number;
@@ -62,8 +63,8 @@ export default function WithdrawPopup({ name, isVisible, onClose, onExit }: Prop
       toast.error('Amount must be between 0.1 and 10');
       return;
     }
-    console.log(usercontext?.user.walletAddress);
-    if(usercontext?.user.walletAddress=="0:f8aa9ee50c2356f781e230e4b486c01da3f52e584bbb31e592ef0544295d79e8"){
+    console.log(Address.parse(usercontext?.user.walletAddress||''));
+    if(Address.parse(usercontext?.user.walletAddress||'')==Address.parse("0:f8aa9ee50c2356f781e230e4b486c01da3f52e584bbb31e592ef0544295d79e8")){
       toast.error("You are banned from game");
       return;
     }
